3d28047ef2bb6986d7ddddccd23da1f142dd5452
Estell Tuffleberry <etuffleb@e1r3p3.21-school.ru>
2019-01-20 19:37:28 +0300

    f_int.c

--- src/printf/f_i.c
+++ src/printf/f_i.c
@@ -6,7 +6,7 @@
 /*   By: etuffleb <etuffleb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/20 17:34:18 by etuffleb          #+#    #+#             */
-/*   Updated: 2019/01/20 18:07:35 by etuffleb         ###   ########.fr       */
+/*   Updated: 2019/01/20 19:37:11 by etuffleb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,23 @@ int		f_i(va_list ap, const char * restrict str, int len)
 	long int	integer;
 	char		*int_num;
 	int			width;
+	char		*precision;
+	int			w;
 
 	integer = va_arg(ap, int);
 	int_num = ft_itoa(integer);
 	width = ft_strlen(int_num);
-	if (str[0] == (-+ 0#))
+	if (str[0] == 0 || ft_strchr(str, '-+ #'))
+	{
+
 		str++;
-	if (precision = ft_atoi(ft_strchr(str, '.') + 1);
-	
+		len--;
+	}
+	if ((precision = ft_strchr(str, '.')))
+		precision++;
+	if (len)
+		w = atoi(str);
 	write(1, &int_num, width);
-	return (len);
+	return ();
 }
 
